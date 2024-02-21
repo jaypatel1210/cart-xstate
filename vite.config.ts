e5f97168cd5cfdb53340b2addd173fd3bff67e57
jaypatel1210 <jd4215124@gmail.com>
2024-02-21 11:09:28 +0530

    chore: vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/main.tsx',
+        template1: './src/templates/Template1.tsx',
+      },
+      output: {
+        entryFileNames(chunkInfo) {
+          if (chunkInfo.name != 'main') {
+            return `assets/${chunkInfo.name}.js`;
+          }
+          return 'assets/[name]-[hash].js';
+        },
+      },
+    },
+  },
+});
